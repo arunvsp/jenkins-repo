79a9161a1b8dd8c43a2cf6a5d816573b35f24cb8
Arun_vsp <arunvsp1998@gmail.com>
2020-12-21 14:58:32 +0530

    added diff method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -1,12 +1,14 @@
 package com.lti;
 
 public class Calc {
-public static int add(int num1,int num2)
-{
-	return num1+num2;
+	public static int add(int num1, int num2) {
+		return num1 + num2;
 	}
-	public static void main(String args[])
-	{
-		System.out.println(add(10,20));
+	public static int diff(int num1, int num2) {
+		return num1 - num2;
+	}
+	public static void main(String args[]) {
+		System.out.println(add(10, 20));
+		System.out.println(diff(100, 20));
 	}
 }
